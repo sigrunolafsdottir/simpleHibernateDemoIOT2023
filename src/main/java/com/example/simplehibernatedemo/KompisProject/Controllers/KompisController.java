18909507278a7c81266c9922e959c7d513047671
sigrunolafsdottir <solafsdottir@gmail.com>
2023-09-11 20:28:06 +0200

    Fler CRUD-funktioner tillagda

--- src/main/java/com/example/simplehibernatedemo/KompisProject/Controllers/KompisController.java
+++ src/main/java/com/example/simplehibernatedemo/KompisProject/Controllers/KompisController.java
@@ -2,9 +2,12 @@ package com.example.simplehibernatedemo.KompisProject.Controllers;
 
 import com.example.simplehibernatedemo.KompisProject.Models.Kompis;
 import com.example.simplehibernatedemo.KompisProject.Repo.KompisRepo;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class KompisController {
@@ -21,5 +24,26 @@ public class KompisController {
         Kompis newFriend = new Kompis(id, name, mail, mobile);
         kr.save(newFriend);
         return "Friend saved";
+    }
+
+    @RequestMapping("/findAll")
+    public List<Kompis> findAll(){
+        return kr.findAll();
+    }
+
+    @RequestMapping("/findById/{id}")
+    public Kompis findById(@PathVariable Long id){
+        return kr.findById(id).orElse(null);
+    }
+
+    @RequestMapping("/findByName/{name}")
+    public List<Kompis> findById(@PathVariable String name){
+        return kr.findByName(name);
+    }
+
+    @RequestMapping("/deleteById/{id}")
+    public String deleteById(@PathVariable Long id){
+        kr.deleteById(id);
+        return "Kompis med id "+id+" är borttagen";
     }
 }
